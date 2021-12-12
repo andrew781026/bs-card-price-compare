0c47d09d6f006e2f2353b6404c479dde8c7d8375
andrew <andrew7810262000@gmail.com>
2021-12-13 07:20:25 +0800

    add serverless-http using

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const {initLineService} = require("./router/line");
 
+const path = require('path');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+
 // Initialize Express
 const app = express();
+
+app.use(bodyParser.json());
 
 initLineService({app})
 
@@ -17,3 +23,20 @@ app.listen(5000, () => {
 });
 
 // line-bot 的最小範例 : https://line.github.io/line-bot-sdk-nodejs/getting-started/basic-usage.html#synopsis
+
+/*
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+ */
+
+// app.use('/.netlify/functions/server', router);  // path must route to lambda
+// app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
