ef3ac7ff7d8eff9e814c42d8008a495a94ace782
andrew_wang <andrew_wang@friday.tw>
2021-12-14 11:09:50 +0800

    移除 @sentry/tracing 相關使用 , 先將 errorHandler 測試通過

--- src/sentry/init.js
+++ src/sentry/init.js
@@ -2,36 +2,15 @@
 require('dotenv').config({path: '../../.env'});
 
 const Sentry = require('@sentry/node');
-const Tracing = require("@sentry/tracing");
 
 const Sentry_dsn = process.env.Sentry_dsn;
 
 const initSentry = ({app}) => {
 
     Sentry.init({
-        debug: true,
         // dsn 從右側連結取得 : https://sentry.io/settings/ezoom/projects/bs-card-price-compare/keys/
         dsn: Sentry_dsn,
-        integrations: [
-            // enable HTTP calls tracing
-            new Sentry.Integrations.Http({tracing: true}),
-            // enable Express.js middleware tracing
-            new Tracing.Integrations.Express({app}),
-        ],
-
-        // Set tracesSampleRate to 1.0 to capture 100%
-        // of transactions for performance monitoring.
-        // We recommend adjusting this value in production
-        tracesSampleRate: 1.0,
     });
-
-    // Sentry.captureException(new Error('test exception'));
-
-    // RequestHandler creates a separate execution context using domains, so that every
-    // transaction/span/breadcrumb is attached to its own Hub instance
-    app.use(Sentry.Handlers.requestHandler());
-    // TracingHandler creates a trace for every incoming request
-    app.use(Sentry.Handlers.tracingHandler());
 
     // The error handler must be before any other error middleware and after all controllers
     app.use(Sentry.Handlers.errorHandler());
@@ -44,7 +23,7 @@ const initSentry = ({app}) => {
     app.use(function onError(err, req, res, next) {
 
         // send the error message to Sentry
-        Sentry.captureException(err);
+        // Sentry.captureException(err);
 
         // The error id is attached to `res.sentry` to be returned
         // and optionally displayed to the user for support.
